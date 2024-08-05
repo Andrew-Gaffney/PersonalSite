1c2d02274af6ee93d95cf99f0e9c4ebc57bf21c5
Andrew Gaffney <andrew.atreides@gmail.com>
2024-08-05 19:22:15 -0400

    Updated listening port

--- server/index.js
+++ server/index.js
@@ -12,6 +12,6 @@ app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000.')
 })
